42dad98664769b6f3e1c67098440f64973c7a155
Mark Janssen -- Sig-I/O Automatisering <mark@sig-io.nl>
2019-11-18 16:04:40 +0100

    Fix when working with subdomains

--- hook-dns-openprovider.py
+++ hook-dns-openprovider.py
@@ -3,57 +3,55 @@
 import sys
 import requests
 import json
-import os
 
 argc = len(sys.argv)
 
 if ( argc != 5):
+    print("Wrong number of arguments:",argc)
     sys.exit(0)
 
+
+# Bearer-token is created with get-token... needs to be an account without 2FA AFAIK
 api = "https://api.openprovider.eu/v1beta"
+bearer = "XXXXXXXXXXXXXXXXXX"
 acme = "_acme-challenge"
 ttl = "600"
 handler = sys.argv[1]
 domain = sys.argv[2]
 challenge = sys.argv[4]
+subdomain = ""
+superdomain = ""
 
-# Try logging in to get a bearer-token
+# Find the base domain for the given domainname
 try:
-    bearer = os.environ['OPENPROVIDER_API_TOKEN']
+    zonelist = ['your-domain.tls', 'other-domain.tld', 'example.com']
+    for zone in zonelist:
+        if zone in domain:
+            subdomain = domain.split(zone)[0][:-1]
+            superdomain = zone
+            print(subdomain, superdomain)
+            if (subdomain != ""):
+                acme = acme + "." + subdomain
 except:
-    username = os.environ['OPENPROVIDER_API_USERNAME']
-    password = os.environ['OPENPROVIDER_API_PASSWORD']
+    # Domain not found in the list, assuming it's a normal domain without subdomains
+    subdomain = ""
+    superdomain = domain
 
-    sourceip = "0.0.0.0"
-    postdata = { "username": username, "password": password, "ip": sourceip  }
-    resp = requests.post(url=api + "/auth/login", data = json.dumps(postdata) )
-
-    if (resp.ok):
-        data = json.loads(resp.text)
-        bearer = data['data']['token']
-    else:
-        print("Couldn't get a bearer-token, password incorrect?")
-        sys.exit(20)
 
 headers = { "Content-Type": "application/json", "Authorization": "Bearer " + bearer }
 
 if ( handler == 'deploy_challenge'):
     add = [ { 'type': 'TXT', 'name': acme, 'value': challenge, 'ttl': ttl } ]
-    postdata = {'name': domain, 'records': { 'add': add } }
+    postdata = {'name': superdomain, 'records': { 'add': add } }
 elif ( handler == 'clean_challenge'):
-    delchal= '"' + challenge + '"'  # I think the OpenProvider API is broken here...
+    delchal= '"' + challenge + '"'
     remove = [ { 'type': 'TXT', 'name': acme, 'value': delchal, 'ttl': ttl, 'creation_date': "", 'modification_date': ""} ]
-    postdata = {'name': domain, 'records': { 'remove': remove } }
+    postdata = {'name': superdomain, 'records': { 'remove': remove } }
 else:
     sys.exit(0)
 
-resp = requests.put(url=api + "/dns/zones/" + domain, data=json.dumps(postdata), headers=headers )
+resp = requests.put(url=api + "/dns/zones/" + superdomain, data=json.dumps(postdata), headers=headers )
 print(resp.json())
-responsedata = json.loads(resp.text)
 
-if (resp.ok == True):
-    sys.exit(0)
-elif ( responsedata['desc'] == "Duplicate record" ):
-    sys.exit(0)
-else:
-    sys.exit(1)
+sys.exit(resp.ok)
+
