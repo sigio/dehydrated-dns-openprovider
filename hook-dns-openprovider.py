4fefcd8356c618f67b54edcfab376bddb674c7a6
Mark Janssen -- Sig-I/O Automatisering <mark@sig-io.nl>
2020-01-27 21:50:26 +0100

    Major update
    Tested and working with:
        - Wildcard ( *.mydomain.tld)
        - Normal subdomain ( something.mydomain.tld )
        - Bare domain ( mydomain.tld )
        - Spam subdomains ( some.very.deep.domain.mydomain.tld )
    
    Domain is checked against your hosted domains @ openprovider,
    fqdn is split between domain bit and subdomain.
    
    Will use bearer-token from environment if given, otherwise fall back
    on getting a token with username and password (not recommended)

--- hook-dns-openprovider.py
+++ hook-dns-openprovider.py
@@ -1,56 +1,105 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import sys
 import requests
 import json
+import os
 
 argc = len(sys.argv)
 
-if ( argc != 5):
-    print("Wrong number of arguments:",argc)
+if ( argc < 5):
     sys.exit(0)
 
-
-# Bearer-token is created with get-token... needs to be an account without 2FA AFAIK
 api = "https://api.openprovider.eu/v1beta"
-bearer = "XXXXXXXXXXXXXXXXXX"
-acme = "_acme-challenge"
+acme_str = "_acme-challenge"
+acme = acme_str
 ttl = "600"
 handler = sys.argv[1]
-domain = sys.argv[2]
-challenge = sys.argv[4]
-subdomain = ""
-superdomain = ""
+hostdomain = sys.argv[2]
 
-# Find the base domain for the given domainname
+if ( handler == 'deploy_cert'):
+    print( " * Deploy-cert\n")
+    privkey_pem, cert_pem, fullchain_pem, chain_pem, timestamp = sys.argv[3:]
+    filenames = [privkey_pem, fullchain_pem]
+    with open('/var/lib/dehydrated/chains/' + str.replace(hostdomain, '*', 'wildcard') + '.pem', 'w') as outfile:
+      for fname in filenames:
+        with open(fname) as infile:
+          outfile.write(infile.read())
+    sys.exit(0)
+
+challenge = sys.argv[4]
+
+# Try logging in to get a bearer-token
 try:
-    zonelist = ['your-domain.tls', 'other-domain.tld', 'example.com']
-    for zone in zonelist:
-        if zone in domain:
-            subdomain = domain.split(zone)[0][:-1]
-            superdomain = zone
-            print(subdomain, superdomain)
-            if (subdomain != ""):
-                acme = acme + "." + subdomain
+    bearer = os.environ['OPENPROVIDER_API_TOKEN']
 except:
-    # Domain not found in the list, assuming it's a normal domain without subdomains
-    subdomain = ""
-    superdomain = domain
+    username = os.environ['OPENPROVIDER_API_USERNAME']
+    password = os.environ['OPENPROVIDER_API_PASSWORD']
+
+    sourceip = "0.0.0.0"
+    postdata = { "username": username, "password": password, "ip": sourceip  }
+    resp = requests.post(url=api + "/auth/login", data = json.dumps(postdata) )
+
+    if (resp.ok):
+        data = json.loads(resp.text)
+        bearer = data['data']['token']
+    else:
+        print("Couldn't get a bearer-token, password incorrect?")
+        sys.exit(20)
 
 headers = { "Content-Type": "application/json", "Authorization": "Bearer " + bearer }
 
+
+# Rest only deals with deploy or clean_challenge
+if ( handler != 'deploy_challenge') and ( handler != 'clean_challenge'):
+    sys.exit(0)
+
+# If we are dealing with a wildcard, remove this section
+wildcard = False
+if ( hostdomain.split(".",1)[0] == "*" ):
+    wildcard = True
+    hostdomain = "".join(hostdomain.split(".",1)[1:])
+
+try:
+    # Find split between host and zone part
+    domain = hostdomain
+    record = ""
+    dots = 0;
+    resp = requests.get(url=api + "/dns/zones/" + domain, headers=headers )
+    while not resp:
+        print( "Domain " + domain + " not found, splitting further\n" )
+        dots += 1
+        domain = hostdomain.split(".", dots)[dots]
+        record = ".".join(hostdomain.split(".")[0:dots])
+        resp = requests.get(url=api + "/dns/zones/" + domain, headers=headers )
+    print( "Domain " + domain + " found in your DNS zones. So record is " + record + "\n" )
+    if ( dots >= 1):
+        acme = acme_str + "." + ".".join(hostdomain.split(".", dots)[0:dots])
+    else:
+        acme = acme_str 
+except:
+    print( "Couldn't find your zone\n" )
+    sys.exit(21)
+    
 if ( handler == 'deploy_challenge'):
     add = [ { 'type': 'TXT', 'name': acme, 'value': challenge, 'ttl': ttl } ]
-    postdata = {'name': superdomain, 'records': { 'add': add } }
+    postdata = {'name': domain, 'records': { 'add': add } }
 elif ( handler == 'clean_challenge'):
-    delchal= '"' + challenge + '"'
+    delchal= '"' + challenge + '"'  # I think the OpenProvider API is broken here...
     remove = [ { 'type': 'TXT', 'name': acme, 'value': delchal, 'ttl': ttl, 'creation_date': "", 'modification_date': ""} ]
-    postdata = {'name': superdomain, 'records': { 'remove': remove } }
+    postdata = {'name': domain, 'records': { 'remove': remove } }
 else:
     sys.exit(0)
 
-resp = requests.put(url=api + "/dns/zones/" + superdomain, data=json.dumps(postdata), headers=headers )
+print( "Updating zone with domain " + domain + ", acme: " + acme + ", challenge: " + challenge + "\n")
+resp = requests.put(url=api + "/dns/zones/" + domain, data=json.dumps(postdata), headers=headers )
 print(resp.json())
+responsedata = json.loads(resp.text)
 
-sys.exit(0)
-
+if (resp.ok == True):
+    sys.exit(0)
+elif ( responsedata['desc'] == "Duplicate record" ):
+    sys.exit(0)
+else:
+    sys.exit(1)
